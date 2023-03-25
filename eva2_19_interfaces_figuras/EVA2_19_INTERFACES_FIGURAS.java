5e3fe1c7138bb4ac6c63d537ca5ee21aeae9cb53
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-03-24 18:28:23 -0600

    Add files via upload

---
+++ eva2_19_interfaces_figuras/EVA2_19_INTERFACES_FIGURAS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva2_19_interfaces_figuras;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA2_19_INTERFACES_FIGURAS {
+
+    public static void main(String[] args) {
+        Circulo circu = new Circulo(20);
+        circu.imprimirDatos();
+        Triángulo trian = new Triángulo(20,20);
+        trian.imprimirDatos();
+    }
+}
